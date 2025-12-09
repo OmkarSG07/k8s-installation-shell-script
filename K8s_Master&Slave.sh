73f1515ebcd12992891403f7d1845a2e4ce7261b
OmkarSG07 <Omkarsg.7781@gmail.com>
2025-12-09 16:08:26 +0530

    Update Kubernetes version to 1.32.1

--- K8s_Master&Slave.sh
+++ K8s_Master&Slave.sh
@@ -12,12 +12,12 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
 sudo mkdir -p -m 755 /etc/apt/keyrings
 
 #Add Kubernetes Repository and GPG Key[On Master & Worker Node]     !!! check for latest stable realease versions  !!!
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 #Update Package List[On Master & Worker Node]
 sudo apt update
 
 #Install Kubernetes Components[On Master & Worker Node]    !!! check for latest stable realease versions  !!!
-sudo apt install -y kubeadm=1.28.1-1.1 kubelet=1.28.1-1.1 kubectl=1.28.1-1.1
+sudo apt install -y kubeadm=1.32.1-1.1 kubelet=1.32.1-1.1 kubectl=1.32.1-1.1
 
